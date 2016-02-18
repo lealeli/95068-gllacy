8f0ac630e8ad2dbf8b9a97bd5dee1b003b0cfc76
lealeli <elenamitchenko@yandex.ru>
2016-02-18 10:34:57 +0300

    Шрифты, карта

---
+++ js/map.js
@@ -0,0 +1,19 @@
+(function (window) {
+
+  window.initMap = function () {
+    var myLatLng = {lat: 34.8697395, lng: -111.76098960000002};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: myLatLng,
+      disableDefaultUI: true
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      icon: 'img/pin.png'
+    });
+  }
+
+})(window);
